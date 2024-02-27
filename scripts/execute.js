60790953c9e7f2b57c6952d85006511d685b9a5b
David Zhang <d12stan@hotmail.com>
2024-02-27 22:32:07 +0700

    submit code

--- scripts/execute.js
+++ scripts/execute.js
@@ -1,56 +1,55 @@
-const hre = require("hardhat");
+const hre = require("hardhat")
 
-const FACTORY_NONCE = 1;
-const FACTORY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-const EP_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const FACTORY_NONCE = 1
+const FACTORY_ADDRESS = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
+const EP_ADDRESS = "0x0165878A594ca255338adfa4d48449f69242Eb8F"
+const PM_ADDRESS = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853"
 
 async function main() {
-  const entryPoint = await hre.ethers.getContractAt("EntryPoint", EP_ADDRESS);
+    const entryPoint = await hre.ethers.getContractAt("EntryPoint", EP_ADDRESS)
 
-  // CREATE = hash(deployer, nonce)
-  const sender = await hre.ethers.getCreateAddress({
-    from: FACTORY_ADDRESS,
-    nonce: FACTORY_NONCE,
-  });
-  console.log(sender);
-  // 0xCafac3dD18aC6c6e92c921884f9E4176737C052c
+    // CREATE = hash(deployer, nonce)
+    const sender = await hre.ethers.getCreateAddress({
+        from: FACTORY_ADDRESS,
+        nonce: FACTORY_NONCE,
+    })
+    console.log({ sender })
+    // 0xCafac3dD18aC6c6e92c921884f9E4176737C052c
 
-  const AccountFactory = await hre.ethers.getContractFactory("AccountFactory");
-  const [signer0] = await hre.ethers.getSigners();
-  const address0 = await signer0.getAddress();
-  const initCode = "0x";
-  // FACTORY_ADDRESS +
-  // AccountFactory.interface
-  //   .encodeFunctionData("createAccount", [address0])
-  //   .slice(2);
-  console.log(initCode);
+    const AccountFactory = await hre.ethers.getContractFactory("AccountFactory")
+    const [signer0] = await hre.ethers.getSigners()
+    const address0 = await signer0.getAddress()
+    const initCode = "0x"
+    //     FACTORY_ADDRESS +
+    //     AccountFactory.interface.encodeFunctionData("createAccount", [address0]).slice(2)
+    // console.log(initCode)
 
-  // await entryPoint.depositTo(sender, {
-  //   value: hre.ethers.parseEther("100"),
-  // });
+    // await entryPoint.depositTo(PM_ADDRESS, {
+    //     value: hre.ethers.parseEther("100"),
+    // })
 
-  const Account = await hre.ethers.getContractFactory("Account");
+    const Account = await hre.ethers.getContractFactory("Account")
 
-  const userOp = {
-    sender: sender, // smart contract address
-    nonce: await entryPoint.getNonce(sender, 0),
-    initCode,
-    callData: Account.interface.encodeFunctionData("execute"),
-    callGasLimit: 200_000,
-    verificationGasLimit: 200_000,
-    preVerificationGas: 50_000,
-    maxFeePerGas: hre.ethers.parseUnits("10", "gwei"),
-    maxPriorityFeePerGas: hre.ethers.parseUnits("5", "gwei"),
-    paymasterAndData: "0x",
-    signature: "0x",
-  };
+    const userOp = {
+        sender: sender, // smart contract address
+        nonce: await entryPoint.getNonce(sender, 0),
+        initCode,
+        callData: Account.interface.encodeFunctionData("execute"),
+        callGasLimit: 200_000,
+        verificationGasLimit: 200_000,
+        preVerificationGas: 50_000,
+        maxFeePerGas: hre.ethers.parseUnits("10", "gwei"),
+        maxPriorityFeePerGas: hre.ethers.parseUnits("5", "gwei"),
+        paymasterAndData: PM_ADDRESS,
+        signature: "0x",
+    }
 
-  const tx = await entryPoint.handleOps([userOp], address0);
-  const receipt = await tx.wait();
-  console.log(receipt);
+    const tx = await entryPoint.handleOps([userOp], address0)
+    const receipt = await tx.wait()
+    console.log(receipt)
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+    console.error(error)
+    process.exitCode = 1
+})
--- scripts/test.js
+++ scripts/test.js
@@ -1,17 +1,24 @@
-const hre = require("hardhat");
+const hre = require("hardhat")
 
-const ACCOUNT_ADDRESS = "0xCafac3dD18aC6c6e92c921884f9E4176737C052c";
+const ACCOUNT_ADDRESS = "0x61c36a8d610163660E21a8b7359e1Cac0C9133e1"
+const EP_ADDRESS = "0x0165878A594ca255338adfa4d48449f69242Eb8F"
+const PM_ADDRESS = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853"
 
 async function main() {
-  const account = await hre.ethers.getContractAt("Account", ACCOUNT_ADDRESS);
-  const count = await account.count();
+    const account = await hre.ethers.getContractAt("Account", ACCOUNT_ADDRESS)
+    const count = await account.count()
 
-  //   const code = await hre.ethers.provider.getCode(EP_ADDR);
+    //   const code = await hre.ethers.provider.getCode(EP_ADDR);
+    console.log(count)
 
-  console.log(count);
+    console.log("account balance", await hre.ethers.provider.getBalance(ACCOUNT_ADDRESS))
+
+    const ep = await hre.ethers.getContractAt("EntryPoint", EP_ADDRESS)
+    console.log("account balance on EP", await ep.balanceOf(ACCOUNT_ADDRESS))
+    console.log("paymaster balance on EP", await ep.balanceOf(PM_ADDRESS))
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+    console.error(error)
+    process.exitCode = 1
+})
